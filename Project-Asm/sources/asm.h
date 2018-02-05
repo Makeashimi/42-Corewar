2e76cf3d0f75c6a745897ae6f497a7b35037c81d
Jennifer CHARLOIS <jcharloi@e1r9p17.42.fr>
2018-02-05 21:50:39 +0100

    Need to get all instructions

--- Project-Asm/sources/asm.h
+++ Project-Asm/sources/asm.h
@@ -6,7 +6,7 @@
 /*   By: jcharloi <jcharloi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/18 16:57:38 by jcharloi          #+#    #+#             */
-/*   Updated: 2018/02/03 19:20:02 by jcharloi         ###   ########.fr       */
+/*   Updated: 2018/02/05 21:49:20 by jcharloi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,24 @@
 # include "../../op.h"
 # include "../../libft/libft.h"
 
-typedef struct		s_asm
+typedef struct				s_instruction
 {
-	char			*str;
-	char			*name_instruction;
-	struct s_asm	*next;
-}					t_asm;
+	int						index_instruction;
+	struct s_instruction	*next;
+}							t_instruction;
 
-int					ft_space(char c);
-int					is_all_space(char *str);
-t_asm				*parse_begin(t_asm *l_asm);
-void				parse_instructions(t_asm *l_asm, t_asm *tmp);
-void				error(char *str);
+typedef struct				s_asm
+{
+	char					*str;
+	t_instruction			*instruction;
+	//char					*label;
+	struct s_asm			*next;
+}							t_asm;
+
+int							ft_space(char c);
+int							is_all_space(char *str);
+t_asm						*parse_begin(t_asm *l_asm);
+void						parse_instructions(t_asm *l_asm, t_asm *tmp);
+void						error(char *str);
 
 # endif
