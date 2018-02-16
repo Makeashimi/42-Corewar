e876d19dcc5f0df9ae218a85107e2af07b27ef3a
Valentin RICHARD <varichar@e2r3p17.42.fr>
2018-02-16 17:08:03 +0100

    nb_octet ocp

--- Project-VM/sources/calc_ocp.c
+++ Project-VM/sources/calc_ocp.c
@@ -6,82 +6,85 @@
 /*   By: thbricqu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/13 15:03:38 by thbricqu          #+#    #+#             */
-/*   Updated: 2018/02/13 15:03:41 by thbricqu         ###   ########.fr       */
+/*   Updated: 2018/02/16 17:06:10 by varichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-static void        print_bits(unsigned char octet, char tab[])
+static int	nb_octet(t_cor *p, t_data *d, int i)
 {
-    int        oct;
-    int        div;
-    int        i;
+	int ret;
+	int c;
 
-    i = 0;
-    tab[8] = '\0';
-    div = 128;
-    oct = octet;
-    while (div != 0)
-    {
-        if (div <= oct)
-        {
-            tab[i++] = '1';
-            oct = oct % div;
-        }
-        else
-            tab[i++] = '0';
-        div = div / 2;
-    }
+	ret = 0;
+	c = 1;
+	if (i > 3 || i < 1)
+		return (g_op_tab[(d->arene[p->ptr]) - 1].short_dir);
+	ret += g_op_tab[(p->ptr) - 1].ocp;
+	while (c <= i)
+	{
+		if (p->param[c] == T_REG)
+			ret += 1;
+		else if (p->param[c] == T_DIR && nb_octet(p, d, 0))
+			ret += 4;
+		else
+			ret += 2;
+	}
+	return (ret);
 }
 
-static int	nb_octet(t_cor *p, t_data *d)
+static int	check_ocp(t_cor *p, t_data *d)
 {
-	if(d->arene[p->ptr] == 0x01 || d->arene[p->ptr] == 0x02
-		|| d->arene[p->ptr] == 0x03 || d->arene[p->ptr] == 0x04
-		|| d->arene[p->ptr] == 0x05 || d->arene[p->ptr] == 0x06
-		|| d->arene[p->ptr] == 0x07 || d->arene[p->ptr] == 0x08
-		|| d->arene[p->ptr] == 0x0d || d->arene[p->ptr] == 0x10)
-		return (0);
-	return (1);
+	int i;
+
+	i = 1;
+	while (i < 4)
+	{
+		if ((p->param[i] != (p->param[i] & g_op_tab[(d->arene[p->ptr]) -\
+					1].arg[i])))
+			return (nb_octet(p, d, i));
+	}
+	return (0);
 }
 
 static void	type_param(char tab[], t_cor *proc, t_data *data)
 {
 	int i;
-	int c;
 
-	c = 1;
 	i = 0;
 
-	proc->param[0] = (int)data->arene[proc->ptr];
-	while(c < 5)
+	while(i < 3)
 	{
-		if (tab[i] == '1' && tab[i + 1] == '1')
-			proc->param[c] = 2;
-		else if ((tab[i] == '1' && tab[i + 1] == '0') && proc->param[4] == 0)
-			proc->param[c] = 4;
-		else if ((tab[i] == '1' && tab[i + 1] == '0') && proc->param[4] == 1)
-			proc->param[c] = 2;
-		else if (tab[i] == '0' && tab[i + 1] == '1')
-			proc->param[c] = 1;
-		else if (tab[i] == '0' && tab[i + 1] == '0')
-			proc->param[c] = 0;
-		i = i + 2;
-		c++;
+		if (tab[i] == REG_CODE)
+			proc->param[i + 1] = T_REG;
+		else if (tab[i] == DIR_CODE)
+			proc->param[i + 1] = T_DIR;
+		else if (tab[i] == IND_CODE)
+			proc->param[i + 1] = T_IND;
+		else 
+			proc->param[i + 1]= 0;
+		i++;
 	}
 }
 
-void	cal_ocp(t_cor *proc, t_data *data)
+int		calc_ocp(t_cor *proc, t_data *data)
 {
-	int i;
-	char tab[9];
+	int ret;
+	char tab[3];
 
-	i = proc->ptr;
-	proc->param[4] = nb_octet(proc, data);
-	i++;
-	print_bits(data->arene[i], tab);
+	ret = 0;
+	proc->pc = proc->ptr + 1;
+	proc->param[4] = nb_octet(proc, data, 4);
+	if (g_op_tab[(proc->ptr) - 1].ocp)
+		proc->param[0] = proc->pc;
+	else
+		proc->param[0] = 0;
+	tab[0] = (proc->param[0] & 192) >> 6;
+	tab[1] = (proc->param[0] & 48) >> 4;
+	tab[2] = (proc->param[0] & 12) >> 2;
 	type_param(tab, proc, data);
+	return (ret);
 }
 
 int		rev_end(int val, char size)
--- VM/champs/ex.s
+++ VM/champs/ex.s
@@ -1,10 +1,4 @@
-
-	
-	  
-	  
-
-
-     .name i"zork"
+.name "zork"
 .comment "just a basic living prog"
 		
 l2:	sti	r1,%:live,%0
