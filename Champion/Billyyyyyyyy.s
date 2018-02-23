c21a140b2fa9ecfc226b843e30f1dbf65a51b1ab
Valentin RICHARD <varichar@e1r4p15.42.fr>
2018-02-23 16:10:42 +0100

    zork beaten

--- Champion/Billyyyyyyyy.s
+++ Champion/Billyyyyyyyy.s
@@ -1,9 +1,14 @@
 .name "Billyyyyyyyy"
 .comment "yooooo bitchyyyyyyyy"
 
-start:	sti r1, %:live, %1
-		
-
-
-live:	live %42
-		zjmp :live
+			and %42, %42, r2
+			zjmp %500
+start:		sti r1, %:live, %1
+			sti r1, %:forklive, %1
+			zjmp %2
+			fork %:forklive
+			lfork %2000
+forklive:	live %42
+			fork %:forklive
+live:		live %42
+			zjmp %:live
