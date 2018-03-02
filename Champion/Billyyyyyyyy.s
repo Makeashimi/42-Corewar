4aa4a99f9184d297254d0323bdeb38592901e377
Valentin Richard <varchar@Valentins-Mac.local>
2018-03-02 03:44:27 -0800

    update champion

--- Champion/Billyyyyyyyy.s
+++ Champion/Billyyyyyyyy.s
@@ -1,8 +1,9 @@
 .name "Billyyyyyyyy"
 .comment "yooooo bitchyyyyyyyy"
 
-			and %42, %42, r2
-			zjmp %500
+			
+			and r2, %0, r2
+			zjmp %512
 start:		sti r1, %:live, %1
 			sti r1, %:forklive, %1
 			zjmp %2
