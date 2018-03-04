3d16ea54be4ab647cbda3674988b35f7e67aca92
Valentin RICHARD <varichar@e2r3p8.42.fr>
2018-03-05 00:31:40 +0100

    champ done

--- Champion/Billyyyyyyyy.s
+++ Champion/Billyyyyyyyy.s
@@ -1,15 +1,11 @@
 .name "Billyyyyyyyy"
 .comment "yooooo bitchyyyyyyyy"
 
-			
-			and r2, %0, r2
-			zjmp %512
-start:		sti r1, %:live, %1
-			sti r1, %:forklive, %1
-			zjmp %2
-			fork %:forklive
-			lfork %2000
-forklive:	live %42
-			fork %:forklive
-live:		live %42
-			zjmp %:live
+jump:	sti r1, %:flive, %1
+		sti r1, %:live, %1
+		and r1, %0, r2
+		lfork %2025
+flive:	live	%1
+		fork	%:flive
+live:	live	%1
+		zjmp	%:live
